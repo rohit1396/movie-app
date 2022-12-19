18ef3c461aae28b8567ac7c27b803b04bb2d53d3
Rohit <rohitgaikwad726@gmail.com>
2022-12-19 19:57:46 +0530

    Home comp created

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Home.css"
+import MovieRow from "./MovieRow";
+import requests from "../requests";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <MovieRow
+        title="Neflix Originals"
+        fetchUrl={requests.fetchNetflixoriginals}
+        isLarge={true}
+      />
+      <MovieRow title="Trending" fetchUrl={requests.fetchTrending} />
+      <MovieRow title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <MovieRow title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <MovieRow title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <MovieRow title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <MovieRow
+        title="Romantic Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <MovieRow title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+    </div>
+  );
+};
+
+export default Home;
