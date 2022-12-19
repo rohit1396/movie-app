5f63b7b8a1216e7681b47269b46b37e3083bc7d7
Rohit <rohitgaikwad726@gmail.com>
2022-12-19 19:59:09 +0530

    MovieRow added

---
+++ src/components/MovieRow.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import "./MovieRow.css";
+import axios from "../axios";
+
+const base_url = "https://image.tmdb.org/t/p/original";
+
+const MovieRow = ({ title, fetchUrl, isLarge }) => {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovieData(request.data.results);
+    };
+    getMovies();
+  }, [fetchUrl]);
+
+  return (
+    <div className="movieRow">
+      <h1>{title}</h1>
+      <div className={`movieRow_posters`}>
+        {movieData.map((movie) => {
+          return (
+            // <ul>
+            <img
+              key={movie?.id}
+              className={`movieRow_poster ${isLarge && "movieRow_posterLarge"}`}
+              src={`${base_url}${
+                isLarge ? movie?.poster_path : movie?.backdrop_path
+              }`}
+              alt={`${movie?.title || movie?.original_title}`}
+            />
+            // </ul>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MovieRow;
