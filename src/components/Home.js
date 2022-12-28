9b02af907f37ba2cfa7917185b67c81ab13593ee
Rohit <rohitgaikwad726@gmail.com>
2022-12-28 07:55:07 +0530

    movieCover imported

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
 import MovieRow from "./MovieRow";
 import requests from "../requests";
+import MovieCover from "./MovieCover";
 
 const Home = () => {
   return (
     <div className="home">
+      <MovieCover />
       <MovieRow
         title="Neflix Originals"
         fetchUrl={requests.fetchNetflixoriginals}
