e9547b4d4174ad82f46797a12cab17145c04188d
rohit <rohitgaikwad726@gmail.com>
2023-01-16 20:03:52 +0530

    movie row added

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -3,12 +3,15 @@ import "./MovieRow.css";
 import axios from "../axios";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import MovieCard from "./MovieCard";
 // import { Link } from "react-router-dom";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 const MovieRow = ({ title, fetchUrl, isLarge }) => {
   const [movieData, setMovieData] = useState([]);
+  const [show, setShow] = useState(false);
+  const [getID, setGetID] = useState([]);
 
   useEffect(() => {
     const getMovies = async () => {
@@ -19,6 +22,17 @@ const MovieRow = ({ title, fetchUrl, isLarge }) => {
     getMovies();
   }, [fetchUrl]);
 
+  // console.log(show);
+
+  const showMovieDetails = () => {
+    setShow(true);
+    // setGetID([...getID, idx]);
+    // console.log(idx);
+  };
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
   return (
     <div className="movieRow">
       <h1>{title}</h1>
@@ -28,16 +42,28 @@ const MovieRow = ({ title, fetchUrl, isLarge }) => {
         {movieData.map((movie) => {
           return (
             <>
-              <img
-                key={movie?.id}
-                className={`movieRow_poster ${
-                  isLarge && "movieRow_posterLarge"
-                }`}
-                src={`${base_url}${
-                  isLarge ? movie?.poster_path : movie?.backdrop_path
-                }`}
-                alt={`${movie?.title || movie?.original_title}`}
-              />
+              <MovieCard key={movie.id} movie={movie} isLarge={isLarge} />
+              {/* <div className="movieRow_details">
+                <img
+                  key={movie?.id}
+                  className={`movieRow_poster ${
+                    isLarge && "movieRow_posterLarge"
+                  }`}
+                  onMouseEnter={showMovieDetails}
+                  onMouseLeave={() => setShow(false)}
+                  src={`${base_url}${
+                    isLarge ? movie?.poster_path : movie?.backdrop_path
+                  }`}
+                  alt={`${movie?.title || movie?.original_title}`}
+                />
+                {show && (
+                  <div className="movieRow_description">
+                    <p>{movie?.title || movie?.original_title}</p>
+                    <p>{truncate(movie?.overview, 150)}</p>
+                  <p>{movie?.vote_average}</p>
+                  </div>
+                )}
+              </div> */}
             </>
           );
         })}
