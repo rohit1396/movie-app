d42f87ec8cd7c30749ee683b7eea6647bcf634a0
rohit <rohitgaikwad726@gmail.com>
2023-01-16 20:05:30 +0530

    movie card added

---
+++ src/components/MovieCard.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./MovieCard.css";
+
+const base_url = "https://image.tmdb.org/t/p/original";
+
+const MovieCard = ({ movie, isLarge }) => {
+  const [show, setShow] = useState(false);
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+  return (
+    <div className="movieCard">
+      <img
+        className="movieCard_img"
+        src={`${base_url}${
+          isLarge ? movie?.poster_path : movie?.backdrop_path
+        }`}
+        alt={movie?.title || movie?.original_title}
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+      />
+      {
+        <div
+          className={`${show ? "movieCard_show" : "movieCard_hide"}`}
+          onMouseEnter={() => setShow(true)}
+          onMouseLeave={() => setShow(false)}
+        >
+          <p className="movieCard_title">{movie?.title}</p>
+          <p className="movieCard_overview">{truncate(movie?.overview, 150)}</p>
+          <p className="movieCard_rating">Rating : {movie?.vote_average}</p>
+        </div>
+      }
+    </div>
+  );
+};
+
+export default MovieCard;
