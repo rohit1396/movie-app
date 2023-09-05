72098686539afaa909ce68c1af4366110e593ec6
Rohit <rohitgaikwad726@gmail.com>
2023-09-05 11:22:09 +0530

    importing global state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { MovieProvider } from "./MovieContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <MovieProvider>
+      <App />
+    </MovieProvider>
   </BrowserRouter>
 );
 
