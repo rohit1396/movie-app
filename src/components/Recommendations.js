ed91d89fc8866890488fe39f6cb2d3d69669ecae
Rohit <rohitgaikwad726@gmail.com>
2023-09-08 10:28:46 +0530

    recommendation component added

---
+++ src/components/Recommendations.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./Recommendations.css";
+import { API_KEY } from "../requests";
+import MovieCard from "./MovieCard";
+
+const Recommendations = ({ movieid }) => {
+  console.log(movieid);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const request =
+        await fetch(`https://api.themoviedb.org/3/movie/${movieid}/recommendations?api_key=${API_KEY}&language=en-US&page=1
+            `);
+      const response = await request.json();
+      console.log(response);
+      setMovies(response.results);
+      console.log(movies);
+    };
+    getMovies();
+  }, [movieid]);
+
+  return (
+    <div className="recommendations">
+      {movies?.map((movie) => {
+        return (
+          <sectionn>
+            <MovieCard key={movie.id} movie={movie} />
+          </sectionn>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Recommendations;
