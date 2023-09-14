b98b2fcdd9ba0e461dedad74b96365b4d9bab53f
Rohit <rohitgaikwad726@gmail.com>
2023-09-14 10:39:03 +0530

    Implemented caraousel Functionality and styling

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,48 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./MovieRow.css";
 import axios from "../axios";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import MovieCard from "./MovieCard";
-// import { Link } from "react-router-dom";
-
-const base_url = "https://image.tmdb.org/t/p/original";
 
 const MovieRow = ({ title, fetchUrl, isLarge }) => {
+  const scrollMovie = useRef(null);
   const [movieData, setMovieData] = useState([]);
-  const [show, setShow] = useState(false);
-  const [getID, setGetID] = useState([]);
 
   useEffect(() => {
     const getMovies = async () => {
       const request = await axios.get(fetchUrl);
-      // console.log(request);
       setMovieData(request.data.results);
-      // console.log(movieData);
     };
     getMovies();
   }, [fetchUrl]);
 
-  // console.log(show);
-
-  const showMovieDetails = () => {
-    setShow(true);
-    // setGetID([...getID, idx]);
-    // console.log(idx);
+  const scrollToLeft = () => {
+    if (scrollMovie.current) {
+      scrollMovie.current.scrollLeft -= 500; // Adjust the scroll distance as needed
+    }
+    console.log("left Arrow Clicked");
   };
 
-  const truncate = (str, n) => {
-    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  const scrollToRight = () => {
+    if (scrollMovie.current) {
+      scrollMovie.current.scrollLeft += 500; // Adjust the scroll distance as needed
+    }
+    console.log("Right Arrow Clicked");
   };
+
   return (
     <div className="movieRow">
       <h1>{title}</h1>
       <div className={`movieRow_posters`}>
-        {/* <Link to="/home/moviedetails"> */}
-        {movieData.map((movie) => {
-          return <MovieCard key={movie.id} movie={movie} isLarge={isLarge} />;
-        })}
-        {/* </Link> */}
+        <ArrowBackIosIcon
+          className="movieRow_arrowLeftIcon"
+          onClick={scrollToLeft}
+        />
+        <div className="movieRow_images" ref={scrollMovie}>
+          {movieData.map((movie) => {
+            return (
+              <div className="moviecard">
+                <MovieCard movie={movie} isLarge={isLarge} />
+              </div>
+            );
+          })}
+        </div>
+        <ArrowForwardIosIcon
+          className="movieRow_arrowRightIcon"
+          onClick={scrollToRight}
+        />
       </div>
     </div>
   );
