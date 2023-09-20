4900935accdc3dcaa7419c03d3e6d3d2c1de915c
Rohit <rohitgaikwad726@gmail.com>
2023-09-20 11:47:37 +0530

    adding footer and media queries

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
 import { Link } from "react-router-dom";
 import { GlobalMovie } from "../MovieContext";
 
@@ -25,26 +26,36 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className={`navbar ${show && "navbar_black"}`}>
-      <Link to="/home">
-        <img
-          className="navbar_logo"
-          src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
-          alt="navbar_logo"
-        />
-      </Link>
-      <div className="navbar_search">
-        <input
-          className="navbar_searchInput"
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+    <>
+      <div className={`navbar ${show && "navbar_black"}`}>
+        <Link to="/home">
+          <img
+            className="navbar_logo"
+            src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
+            alt="navbar_logo"
+          />
+        </Link>
+        <div className="navbar_search">
+          <input
+            className="navbar_searchInput"
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Link to="/searchpage">
+            <SearchIcon className="searchIcon" />
+          </Link>
+        </div>
+      </div>
+      <div className="nav_footbar">
+        <Link to="/home">
+          <HomeIcon className="homeIcon" />
+        </Link>
         <Link to="/searchpage">
           <SearchIcon className="searchIcon" />
         </Link>
       </div>
-    </div>
+    </>
   );
 };
 
